c32ac2b84a6b5c36d218e78b17d6043833b54758
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-07-21 01:33:58 +0600

    Refactor genre endpoints to use minimal API with output caching and improve response handling

--- MinimalApiMovies/Program.cs
+++ MinimalApiMovies/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
 using MinimalApiMovies;
@@ -44,52 +45,63 @@ app.UseHttpsRedirection();
 app.UseCors();
 app.UseOutputCache();
 
+var genreMapGroup = app.MapGroup("/genres");
+// .WithTags("Genres")
+// .WithOpenApi();
 
-app.MapGet("/genres",
-        [EnableCors("AllowAllOrigins")] async (IGenreRepository genreRepository) => await genreRepository.GetAll())
+genreMapGroup.MapGet("/", GetGenres).RequireCors("AllowAllOrigins")
     .CacheOutput(x => x.Expire(TimeSpan.FromSeconds(60)).Tag("Get-Genres"));
-
-app.MapGet("/genres/{id:long}", (long id, IGenreRepository genreRepository) =>
-    {
-        return genreRepository.GetById(id)
-            .ContinueWith(task => task.Result is not null ? Results.Ok(task.Result) : Results.NotFound());
-    })
-    .CacheOutput(x => x.Expire(TimeSpan.FromSeconds(60)));
-
-app.MapPost("/genres", async (Genre genre, IGenreRepository genreRepository, IOutputCacheStore store) =>
-{
-    var id = await genreRepository.Add(genre);
-    await store.EvictByTagAsync("Get-Genres", CancellationToken.None);
-    return Results.Created($"/genres/{id}", id);
-});
-
-app.MapPut("/genres/{id:long}",
-    async (long id, Genre genre, IGenreRepository genreRepository, IOutputCacheStore store) =>
-    {
-        if (!await genreRepository.IsExist(id))
-        {
-            return Results.NotFound();
-        }
-
-        genre.Id = id;
-        await genreRepository.Update(genre);
-        await store.EvictByTagAsync("Get-Genres", CancellationToken.None);
-        return Results.NoContent();
-    });
-
-app.MapDelete("/genres/{id:long}",
-    async (long id, IGenreRepository genreRepository, IOutputCacheStore store) =>
-    {
-        if (!await genreRepository.IsExist(id))
-        {
-            return Results.NotFound();
-        }
-
-        await genreRepository.Delete(id);
-        await store.EvictByTagAsync("Get-Genres", CancellationToken.None);
-        return Results.NoContent();
-    });
+genreMapGroup.MapGet("/{id:long}", GetGenreById).CacheOutput(x => x.Expire(TimeSpan.FromSeconds(60)));
+genreMapGroup.MapPost("/", GenreCreate);
+genreMapGroup.MapPut("/{id:long}", GenreUpdate);
+genreMapGroup.MapDelete("/{id:long}", GenreDelete);
 
 #endregion
 
 app.Run();
+return;
+
+static async Task<Ok<List<Genre>>> GetGenres(IGenreRepository genreRepository)
+{
+    return TypedResults.Ok(await genreRepository.GetAll());
+}
+
+static async Task<Results<Ok<Genre>, NotFound>> GetGenreById(long id, IGenreRepository genreRepository)
+{
+    var data = await genreRepository.GetById(id);
+    return data == null ? TypedResults.NotFound() : TypedResults.Ok(data);
+}
+
+static async Task<Created<long>> GenreCreate(Genre genre, IGenreRepository genreRepository, IOutputCacheStore store)
+{
+    var id = await genreRepository.Add(genre);
+    await store.EvictByTagAsync("Get-Genres", CancellationToken.None);
+    return TypedResults.Created($"/genres/{id}", id);
+}
+
+static async Task<Results<NoContent, NotFound>> GenreUpdate(long id, Genre genre, IGenreRepository genreRepository,
+    IOutputCacheStore store)
+{
+    if (!await genreRepository.IsExist(id))
+    {
+        return TypedResults.NotFound();
+    }
+
+    genre.Id = id;
+    await genreRepository.Update(genre);
+    await store.EvictByTagAsync("Get-Genres", CancellationToken.None);
+    return TypedResults.NoContent();
+}
+
+static async Task<Results<NoContent, NotFound>> GenreDelete(long id, IGenreRepository genreRepository,
+    IOutputCacheStore store)
+{
+    if (!await genreRepository.IsExist(id))
+    {
+        return TypedResults.NotFound();
+    }
+
+    await genreRepository.Delete(id);
+    await store.EvictByTagAsync("Get-Genres", CancellationToken.None);
+    return TypedResults.NoContent();
+}
